c95eb59677fc1c7a3246125b559a8568fb645a10
league <hailinday@gmail.com>
2024-04-26 16:59:58 -0700

    Commit

--- _01_creating_objects/_c_turtle_race.py
+++ _01_creating_objects/_c_turtle_race.py
@@ -37,27 +37,105 @@ if __name__ == '__main__':
     # TODO 1) Create an empty list of turtles
     t_list = list()
     # TODO 2) Create a new turtle and set its shape to 'turtle
-    t = turtle.Turtle()
-    t.shape('turtle')
+
     # TODO 3) Set the turtle's speed to 3
-    t.speed(3)
+
     # TODO 4) Set the turtle's pen up
-    t.penup()
+
     # TODO 5) Use the turtle's goto() method to set its position on the left
     #  side of the screen
-    t.goto(-416, y)
+
     # TODO 6) use a loop to repeat the previous instructions and create
     #  8 turtles lined up on the left side of the screen
     #  *HINT* click on the window to print the corresponding x, y location
-    for i in range(7):
-        y -= 56
+    color_1 = 'red'
+    color_2 = 1
+    for i in range(8):
+        if color_2 == 1:
+            color_1 = 'red'
+        if color_2 == 2:
+            color_1 = 'orange'
+        if color_2 == 3:
+            color_1 = 'yellow'
+        if color_2 == 4:
+            color_1 = 'green'
+        if color_2 == 5:
+            color_1 = 'blue'
+        if color_2 == 6:
+            color_1 = 'purple'
+        if color_2 == 7:
+            color_1 = 'white'
+        if color_2 == 8:
+            color_1 = 'black'
+        color_2 += 1
         t_list.append(turtle.Turtle())
         t_list[i].shape('turtle')
-        t_list[i].speed(3)
+        t_list[i].speed(0)
         t_list[i].penup()
         t_list[i].goto(-416, y)
-    # TODO 7) Move each turtle forward a random distance between 1 and 20
+        t_list[i].color(color_1)
+        y -= 56
 
+    # TODO 7) Move each turtle forward a random distance between 1 and 20
+    completed = False
+    x = None
+    winner = None
+    while not completed:
+        for i in range(8):
+            t_list[i].forward(random.randint(-10, 20))
+            x = t_list[i].xcor()
+            if x > 349:
+                completed = True
+                winner = i
+                if winner == 0:
+                    winner = 'Red'
+                if winner == 1:
+                    winner = 'Orange'
+                if winner == 2:
+                    winner = 'Yellow'
+                if winner == 3:
+                    winner = 'Green'
+                if winner == 4:
+                    winner = 'Blue'
+                if winner == 5:
+                    winner = 'Purple'
+                if winner == 6:
+                    winner = 'White'
+                if winner == 7:
+                    winner = 'Black'
+    for i in range(10):
+        print(winner, "won!")
+    if winner == 'Red':
+        t_list[0].goto(0, 0)
+        t_list[0].shapesize(20)
+    if winner == 'Orange':
+        t_list[1].goto(0, 0)
+        t_list[1].shapesize(20)
+        t_list[1].right(1080)
+    if winner == 'Yellow':
+        t_list[2].goto(0, 0)
+        t_list[2].shapesize(20)
+        t_list[2].right(1080)
+    if winner == 'Green':
+        t_list[3].goto(0, 0)
+        t_list[3].shapesize(20)
+        t_list[3].right(1080)
+    if winner == 'Blue':
+        t_list[4].goto(0, 0)
+        t_list[4].shapesize(20)
+        t_list[4].right(1080)
+    if winner == 'Purple':
+        t_list[5].goto(0, 0)
+        t_list[5].shapesize(20)
+        t_list[5].right(1080)
+    if winner == 'White':
+        t_list[6].goto(0, 0)
+        t_list[6].shapesize(20)
+        t_list[6].right(1080)
+    if winner == 'Black':
+        t_list[7].goto(0, 0)
+        t_list[7].shapesize(20)
+        t_list[7].right(1080)
     # TODO 8) Create a loop to keep moving each turtle until a turtle
     #  crosses the finish line
     #  *HINT* click on the window to print the corresponding x, y location
